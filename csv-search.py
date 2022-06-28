9546180e586d08a115d668f9c6dc66273c028c09
Jacob Martinson <jacobm3@gmail.com>
2022-06-28 14:52:44 -0500

    Update csv-search.py

--- csv-search.py
+++ csv-search.py
@@ -2,24 +2,49 @@
 
 import pandas as pd
 
-
 # Create dataframe from CSV
+# This comes from Google Sheet 'Request List - Read Only' 
+# Save the 'Requests' tab to 'fr.csv'
 df = pd.read_csv('fr.csv', na_filter = '')
 
-df= df[ df['Product'].str.contains('Vault',case=False) ]
-df= df[ df['New or Existing'].str.contains('New', case=False) ]
+print(len(df), 'initial rows')
 
+# Filter down to just Vault requests
+df= df[ df['Product'].str.contains('Vault',case=False) ]
+print(len(df), 'rows Product=Vault')
+
+# Only look at the new requests, not the additional customer subtasks on existing tasks
+df= df[ df['New or Existing'].str.contains('New', case=False) ]
+print(len(df), 'rows only New')
+
+# Text string used to search
 search_str = 'azure'
 
+# Return any tasks that have that string in the title or the currently happens or desired fields.
 df= df[ df['Feature Request Title'].str.contains(search_str,case=False) | \
         df['What Currently Happens'].str.contains(search_str,case=False) | \
         df['What is Desired'].str.contains(search_str,case=False) ]
 
+print(len(df), 'rows searching for %s' % search_str)
 
-    
-df = df[ ['Feature Request Title', 'What Currently Happens','What is Desired', 
-          'Asana Task ID','Asana Sub Task ID']]
+# Filter previous results down to only include results that also have an additional string
+search_str = 'storage'
+df= df[ df['Feature Request Title'].str.contains(search_str,case=False) | \
+        df['What Currently Happens'].str.contains(search_str,case=False) | \
+        df['What is Desired'].str.contains(search_str,case=False) ]
 
-df['Asana Link'] = "https://app.asana.com/0/112957393038545/" + df['Asana Task ID'] 
+print(len(df), 'rows searching for %s' % search_str)
+print()
 
-df.to_excel('fr-search-output.xlsx', index=None, header=True)
+# Create a new dataframe with a subset of the columns we're interested in
+df = df[ ['Feature Request Title', 'What Currently Happens', 'What is Desired', 'Asana Task ID']]
+
+# Add a direct link to the task
+df['Asana Link'] = '=HYPERLINK("https://app.asana.com/0/112957393038545/' + df['Asana Task ID'] + '", "link")'
+
+print('Dataframe Head:')
+print(df.head())
+
+outputfile='fr-search-output.xlsx'
+print('Writing new spreadsheet: %s' % outputfile)
+df.to_excel(outputfile, index=None, header=True)
